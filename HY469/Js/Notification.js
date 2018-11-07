b6b504ba3d44ae9cdf0f2f04770b691e31b6f2d4
paulzik <paulzik-93@hotmail.com>
2018-11-07 16:20:47 +0200

    Annotation Finished (need user icon)

--- HY469/Js/Notification.js
+++ HY469/Js/Notification.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
-
-
-    $(".Footer > .btn").click(function(){
-        $(".Footer > .btn").removeClass("active");
-        $(this).addClass("active");
+    /* Side Panel: Previous Button */
+    $('#ExitBtn').click(function(){
+        $('#ExitButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
+        $('#AnnotationsNotification').hide();
     });
-    
-    
-    
+
+    $('#AcceptBtn').click(function(){
+        $('#AnnotationsNotification').hide();
     });
+
+    $('#DeclineBtn').click(function(){
+        $('#AnnotationsNotification').hide();
+    });
+
+    $("#ExitBtn").mousedown(function(){
+        $('#ExitButtonImg').attr('src', '../Images/Notification/Button_Decline.png'); 
+    });
+    $("#ExitBtn").mouseleave(function(){
+        $('#ExitButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
+    });
+});
