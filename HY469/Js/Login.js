920696a2659e453e7ef96a889cc8449c485ad669
Steve <steveka@hotmail.gr>
2018-11-07 22:32:32 +0200

    Except files, Finished all login screens

--- HY469/Js/Login.js
+++ HY469/Js/Login.js
@@ -4,13 +4,30 @@ $(document).ready(function(){
         this.value = "";
     });
 
-    $("#LoginBtn").focus( function () {
+    $("#LoginBtn").click( function () {
         $('#LoginPart2').show();
         $('#LoginPart1').hide();
     });
 
-    $("#LogoutBtn").focus( function () {
+    $("#LogoutBtn").click( function () {
         $('#LoginPart1').show();
         $('#LoginPart2').hide();
     });
+
+    $("#NewSessionBtn").click( function () {
+        $('#LoginPart3').show();
+        $('#LoginPart2').hide();
+    });
+
+    $("#BackToLoginBtn").click( function () {
+        $('#LoginPart2').show();
+        $('#LoginPart4').hide();
+    });
+
+    $("#SelectSessionBtn").click( function () {
+        $('#LoginPart4').show();
+        $('#LoginPart5').hide();
+    });
+
+
 });
