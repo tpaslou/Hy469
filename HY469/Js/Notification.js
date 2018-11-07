fb87fcdc1579f8b98e9171d94496b9deccca5eeb
paulzik <paulzik-93@hotmail.com>
2018-11-07 17:42:20 +0200

    Coordinator- user options

--- HY469/Js/Notification.js
+++ HY469/Js/Notification.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-    /* Side Panel: Previous Button */
+    /* Annotation Screen */
     $('#ExitBtn').click(function(){
         $('#ExitButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
         $('#AnnotationsNotification').hide();
+        $('#ParticipantOptionsNotification').hide();
     });
 
     $('#AcceptBtn').click(function(){
@@ -19,4 +20,29 @@ $(document).ready(function(){
     $("#ExitBtn").mouseleave(function(){
         $('#ExitButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
     });
+
+    /* Coordinator - UserOptions Screen */
+    $('#MuteUserBtn').click(function(){
+        if ($('#MuteUserButtonImage').attr('src') == "../Images/Notification/Button_Mute.png") {
+            $('#MuteUserButtonImage').attr('src', '../Images/Notification/Button_UnMute.png'); 
+            document.getElementById("MuteTxt").innerHTML = 'Mute';
+        }
+        else{
+            $('#MuteUserButtonImage').attr('src', '../Images/Notification/Button_Mute.png'); 
+            document.getElementById("MuteTxt").innerHTML = 'UnMute';
+        }
+    });
+
+    $('#ExitParticipantOptionsBtn').click(function(){
+        $('#ExitParticipantOptionsButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
+        $('#ParticipantOptionsNotification').hide();
+    });
+
+    $("#ExitParticipantOptionsBtn").mousedown(function(){
+        $('#ExitParticipantOptionsButtonImg').attr('src', '../Images/Notification/Button_Decline.png'); 
+    });
+    $("#ExitParticipantOptionsBtn").mouseleave(function(){
+        $('#ExitParticipantOptionsButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
+    });
+
 });
