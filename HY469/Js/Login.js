fb293f4e3c97c3726e2ef25961faa4dc11b20487
Steve <steveka@hotmail.gr>
2018-11-07 17:53:53 +0200

    3 / 6 Login Screens Done + Conntected

--- HY469/Js/Login.js
+++ HY469/Js/Login.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
         this.value = "";
     });
 
+    $("#LoginBtn").focus( function () {
+        $('#LoginPart2').show();
+        $('#LoginPart1').hide();
+    });
+
+    $("#LogoutBtn").focus( function () {
+        $('#LoginPart1').show();
+        $('#LoginPart2').hide();
+    });
 });
