32a7a29c8de456309c00ea76323e92711343ad82
paulzik <paulzik-93@hotmail.com>
2018-11-08 02:20:51 +0200

    Notes + exit button

--- HY469/Js/MiddlePanel.js
+++ HY469/Js/MiddlePanel.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
 
+    //-----------------Notes----------------
+    $('#ExitNotes').click(function(){
+        $('#ExitNotesButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
+        $('#MiddlePanelTakeNotes').hide();
+    });
+
+    $("#ExitNotes").mousedown(function(){
+        $('#ExitNotesButtonImg').attr('src', '../Images/Notification/Button_Decline.png'); 
+    });
+
+    $("#ExitNotes").mouseleave(function(){
+        $('#ExitNotesButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
+    });
+
+    //-----------------File System----------------
+    $('#ExitFileSystemBtn').click(function(){
+        $('#ExitFileSystemButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
+        $('#MiddlePanelMoreOptionsSelectFile').hide();
+    });
+
+    $("#ExitFileSystemBtn").mousedown(function(){
+        $('#ExitFileSystemButtonImg').attr('src', '../Images/Notification/Button_Decline.png'); 
+    });
+
+    $("#ExitFileSystemBtn").mouseleave(function(){
+        $('#ExitFileSystemButtonImg').attr('src', '../Images/Notification/Button_Exit.png'); 
+    });
 
 });
