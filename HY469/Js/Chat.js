f39307ba2e527f88ef57da6309b0a93bac92a32f
csd3222 <csd3222@csd.uoc.gr>
2018-11-08 11:17:56 +0200

    Mid Panel Core

--- HY469/Js/Chat.js
+++ HY469/Js/Chat.js
@@ -5,15 +5,12 @@
 function openChat(ChatName,elmnt) {
     var i, tabcontent, tablinks;
     var color= "#383838";
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+  
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.backgroundColor = "";
     }
-    document.getElementById(ChatName).style.display = "block";
+    
     elmnt.style.backgroundColor = color;
 
 }
--- HY469/Js/footer.js
+++ HY469/Js/footer.js
@@ -4,3 +4,15 @@ $(document).ready(function() {
     console.log(id);
   });
 });
+
+function openSection(ChatName,elmnt) {
+  var i, tabcontent, tablinks;
+  var color= "#383838";
+  tablinks = document.getElementsByClassName("FT FooterbuttonHeader");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "transparent";
+  }
+  //document.getElementById(ChatName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+
+}
