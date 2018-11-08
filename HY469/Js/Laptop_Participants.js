e7021b26ae1caf8ce0e5134dc4139cc690543c7c
Steve <steveka@hotmail.gr>
2018-11-08 22:05:18 +0200

    Updated Laptop Participant

--- HY469/Js/Laptop_Participants.js
+++ HY469/Js/Laptop_Participants.js
@@ -10,19 +10,45 @@ $(document).ready(function(){
         }
     });
 
-    $('#AnnotationLaptopBtn').click(function () {
-        $('.PerOptionsPanel').hide();
-        $('#AnnotationLaptopPanel').show();
-    });
-
     $('#NotesLaptopBtn').click(function () {
         $('.PerOptionsPanel').hide();
-        $('#NotesLaptopPanel').show();
+        $('#NotificationLaptopPanel').show();
+    });
+
+    $('#AnnotationLaptopBtn').click(function () {
+        /* hide all panels excpet the one pressed */
+        $('.PerOptionsPanel').hide();
+        $('#NotificationLaptopPanel').show();
+        /* hide all notifications excpet the one pressed */
+        $('.NotificationImage').hide();
+        $('#AnnotationNotificationImg').show();
+    });
+
+    $('#PPTChangeLaptopBtn').click(function () {
+        /* hide all panels excpet the one pressed */
+        $('.PerOptionsPanel').hide();
+        $('#NotificationLaptopPanel').show();
+        /* hide all notifications excpet the one pressed */
+        $('.NotificationImage').hide();
+        $('#FileChangeNotificationImg').show();
     });
 
     $('#CoordRequestLaptopBtn').click(function () {
+        /* hide all panels excpet the one pressed */
         $('.PerOptionsPanel').hide();
-        $('#RequestCoordPanel').show();
+        $('#NotificationLaptopPanel').show();
+        /* hide all notifications excpet the one pressed */
+        $('.NotificationImage').hide();
+        $('#CoordinatorNotificationImg').show();
+    });
+
+    $('#ExitNotificationBtn').click(function () {
+        /* hide all panels excpet the one pressed */
+        $('.PerOptionsPanel').hide();
+        $('#AnnotationLaptopPanel').show();
+        /* hide all notifications excpet the one pressed */
+        $('.NotificationImage').hide();
+
     });
     
 });
